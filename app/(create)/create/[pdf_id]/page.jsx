e094bf397b8af65be607a16d4eab791e2acf7598
tanishqkrk <tanishqkrk@gmail.com>
2024-05-07 23:34:57 +0530

    testing out this weird bug (firebase db indexOf)

--- app/(create)/create/[pdf_id]/page.jsx
+++ app/(create)/create/[pdf_id]/page.jsx
@@ -13,6 +13,8 @@ import { FileText } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 import useMotion from "@/context/MotionContext";
 import { AnimatePresence } from "framer-motion";
+import { collection, addDoc, setDoc, doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 export default function Create({ params }) {
   const { userData, uploadToDB } = useData();
   const ID = params.pdf_id;
@@ -20,6 +22,8 @@ export default function Create({ params }) {
   const { M } = useMotion();
 
   const [baseData, setBaseData] = useState({});
+
+  const [content, setContent] = useState([]);
 
   useMemo(() => {
     setBaseData(userData?.pdf_list?.filter((x) => x.id === ID)[0]);
@@ -36,6 +40,38 @@ export default function Create({ params }) {
 
   const addButtonsStyle =
     " flex gap-3 w-full p-1   text-sm items-center dark:text-white text-black p-5 opacity-60 hover:opacity-100 transition-all duration-400";
+
+  function addElement(type) {
+    if (type === "heading") {
+      setContent((prev) => {
+        return {
+          ...prev,
+          content: [
+            ...prev.content,
+            {
+              type: "heading",
+              id: crypto.randomUUID(),
+              heading: "Heading",
+              timestamp: Date.now(),
+              size: 1,
+              fontColor: "#000",
+              backgroundColor: "#fff",
+            },
+          ],
+        };
+      });
+    }
+  }
+
+  async function fetchContent() {
+    const docRef = doc(db, "content", baseData?.content_id);
+    const docSnap = await getDoc(docRef);
+    setContent(docSnap.data());
+  }
+
+  useEffect(() => {
+    return async () => fetchContent();
+  }, []);
 
   return (
     <div className="bg-secondary">
@@ -100,27 +136,60 @@ export default function Create({ params }) {
             }}
             className="element_toolbar flex flex-col  justify-start w-fit items-center  font-semibold bg-secondary fixed right-5 shadow-xl bottom-24 rounded-lg divide-y-2 border-2"
           >
-            <button className={addButtonsStyle}>
+            <button
+              onClick={() => {
+                addElement("heading");
+              }}
+              className={addButtonsStyle}
+            >
               <Heading className="" size={15} />
               <p>Heading</p>
             </button>
-            <button className={addButtonsStyle}>
+
+            <button
+              onClick={() => {
+                addElement("text");
+              }}
+              className={addButtonsStyle}
+            >
               <Type size={15} />
               <p>Text</p>
             </button>
-            <button className={addButtonsStyle}>
+
+            <button
+              onClick={() => {
+                addElement("image");
+              }}
+              className={addButtonsStyle}
+            >
               <Image size={15} />
               <p>Image</p>
             </button>
-            <button className={addButtonsStyle}>
+
+            <button
+              onClick={() => {
+                addElement("table");
+              }}
+              className={addButtonsStyle}
+            >
               <Table size={15} />
               <p>Table</p>
             </button>
-            <button className={addButtonsStyle}>
+            <button
+              onClick={() => {
+                addElement("ulist");
+              }}
+              className={addButtonsStyle}
+            >
               <List size={15} />
               <p>U. List</p>
             </button>
-            <button className={addButtonsStyle}>
+            <button
+              onClick={() => {
+                addElement("olist");
+              }}
+              className={addButtonsStyle}
+            >
               <ListOrdered size={15} />
               <p>O. List</p>
             </button>
@@ -129,7 +198,13 @@ export default function Create({ params }) {
       </AnimatePresence>
 
       <div className="flex justify-center p-12">
-        <div className="h-[1163px] w-[794px] bg-white dark:bg-zinc-800 border-2 "></div>
+        <div className="h-[1163px] w-[794px] bg-white dark:bg-zinc-800 border-2 ">
+          {content?.content?.map((element) => {
+            if (element?.type === "heading") {
+              return <h1 key={element?.id}>{element?.heading}</h1>;
+            }
+          })}
+        </div>
       </div>
     </div>
   );
